90327795929a3c364910bcfbf03bc7e48da4f2d0
Olasunkanmi Oyinlola <olasunkanmi@regovtech.com>
2024-01-24 08:21:21 +0800

    Solana tutorial

--- build/index.js
+++ build/index.js
@@ -1,19 +1,113 @@
 "use strict";
-// class Sorter {
-//   constructor(public collection: number[]) {}
-//   sort(): void {
-//     const { length } = this.collection;
-//     for (let i = 0; i < length; i++) {
-//       for (let j = 0; j < length - i - 1; j++) {
-//         if (this.collection[j] > this.collection[j + 1]) {
-//           const leftHand = this.collection[j];
-//           this.collection[j] = this.collection[j + 1];
-//           this.collection[j + 1] = leftHand;
-//         }
-//       }
-//     }
-//   }
-// }
-// const sorter = new Sorter([1, 5, 3, 2]);
-// sorter.sort();
-// console.log(sorter.collection);
+Object.defineProperty(exports, "__esModule", { value: true });
+const web3_js_1 = require("@solana/web3.js");
+require("dotenv/config");
+const node_helpers_1 = require("@solana-developers/node-helpers");
+class SolanoPlayGround {
+    connection;
+    senderPublicKey;
+    receiverPublicKey;
+    constructor() {
+        this.connection = new web3_js_1.Connection((0, web3_js_1.clusterApiUrl)("devnet"));
+        const senderWallet = process.env.SENDER_PUBLIC_KEY;
+        const receiverWallet = process.env.RECEIVER_PUBLIC_KEY;
+        if (!(senderWallet && receiverWallet)) {
+            throw new Error("provide the sender and receiver wallet address");
+        }
+        this.senderPublicKey = senderWallet;
+        this.receiverPublicKey = receiverWallet;
+    }
+    generateKeyPair() {
+        try {
+            const key = web3_js_1.Keypair.generate();
+            const publicKey = key.publicKey.toBase58();
+            const privateKey = key.secretKey;
+            this.airDrop(publicKey);
+            console.log({ publicKey, privateKey });
+            return { publicKey, privateKey };
+        }
+        catch (error) {
+            console.error("unable to generate key pair.", error);
+        }
+    }
+    async airDrop(publicKey) {
+        const airdrop = await this.connection.requestAirdrop(new web3_js_1.PublicKey(publicKey), web3_js_1.LAMPORTS_PER_SOL * 2);
+        if (airdrop) {
+            console.log(`airdrop success ${airdrop}`);
+        }
+        return airdrop;
+    }
+    async getBalance(publicKey) {
+        try {
+            const address = new web3_js_1.PublicKey(publicKey);
+            const balance = await this.connection.getBalance(address);
+            const sol = balance / web3_js_1.LAMPORTS_PER_SOL;
+            return sol;
+        }
+        catch (error) {
+            console.error("unable to get balance", error);
+        }
+    }
+    getSenderPublicKey() {
+        return this.senderPublicKey;
+    }
+    getReceiverPublicKey() {
+        return this.receiverPublicKey;
+    }
+    getPublicKeyFromSecretKey(key) {
+        return (0, node_helpers_1.getKeypairFromEnvironment)(key).publicKey;
+    }
+    getSecretKey(key) {
+        return (0, node_helpers_1.getKeypairFromEnvironment)(key).secretKey;
+    }
+    async creditWallet() {
+        const senderWallet = this.getSenderPublicKey();
+        const receiverWallet = this.getReceiverPublicKey();
+        const senderSecretKey = this.getSecretKey("SENDER_SECRET_KEY");
+        const x = this.getPublicKeyFromSecretKey("SENDER_SECRET_KEY");
+        try {
+            // Check the balance before initiating the transaction
+            const senderBalanceBefore = await this.getBalance(this.getSenderPublicKey());
+            console.log(`Sender's balance before: ${senderBalanceBefore} SOL`);
+            const lamportsToSend = 5000;
+            console.log("Initiating transaction");
+            const transaction = new web3_js_1.Transaction().add(web3_js_1.SystemProgram.transfer({
+                fromPubkey: x,
+                toPubkey: new web3_js_1.PublicKey(receiverWallet),
+                lamports: lamportsToSend,
+            }));
+            const signature = await (0, web3_js_1.sendAndConfirmTransaction)(this.connection, transaction, [{ publicKey: new web3_js_1.PublicKey(senderWallet), secretKey: senderSecretKey }]);
+            console.log(`💸 Finished! Sent ${lamportsToSend} to the address ${senderWallet} with this signature ${signature}`);
+            // Check the balance after the transaction
+            const senderBalanceAfter = await this.getBalance(this.getSenderPublicKey());
+            console.log(`Sender's balance after: ${senderBalanceAfter} SOL`);
+            return senderBalanceAfter;
+        }
+        catch (error) {
+            console.error("Unable to credit wallet", error);
+        }
+    }
+}
+class Test extends SolanoPlayGround {
+    generate() {
+        return this.generateKeyPair();
+    }
+    async balance() {
+        const balance = await this.getBalance(this.getSenderPublicKey());
+        console.log(balance);
+        return balance;
+    }
+    async credit() {
+        return await this.creditWallet();
+    }
+    async drop() {
+        const publicKey = this.getSenderPublicKey();
+        return await this.airDrop(publicKey);
+    }
+}
+const test = new Test();
+const credit = async () => {
+    return await test.balance();
+};
+// balance();
+credit();
--- src/index.ts
+++ src/index.ts
@@ -10,23 +10,44 @@ import {
 } from "@solana/web3.js";
 import "dotenv/config";
 import { IKeyPair } from "./blockchain.interface";
-import { constants } from "./keys";
 import { getKeypairFromEnvironment } from "@solana-developers/node-helpers";
 
 class SolanoPlayGround {
   connection: Connection;
+  senderPublicKey: string;
+  receiverPublicKey: string;
   constructor() {
     this.connection = new Connection(clusterApiUrl("devnet"));
+    const senderWallet = process.env.SENDER_PUBLIC_KEY;
+    const receiverWallet = process.env.RECEIVER_PUBLIC_KEY;
+    if (!(senderWallet && receiverWallet)) {
+      throw new Error("provide the sender and receiver wallet address");
+    }
+    this.senderPublicKey = senderWallet;
+    this.receiverPublicKey = receiverWallet;
   }
   protected generateKeyPair(): IKeyPair | undefined {
     try {
-      const publicKey = Keypair.generate().publicKey.toBase58();
-      const privateKey = Keypair.generate().secretKey;
+      const key = Keypair.generate();
+      const publicKey = key.publicKey.toBase58();
+      const privateKey = key.secretKey;
+      this.airDrop(publicKey);
       console.log({ publicKey, privateKey });
       return { publicKey, privateKey };
     } catch (error) {
-      console.error("erro", error);
+      console.error("unable to generate key pair.", error);
     }
+  }
+
+  protected async airDrop(publicKey: string) {
+    const airdrop = await this.connection.requestAirdrop(
+      new PublicKey(publicKey),
+      LAMPORTS_PER_SOL * 2
+    );
+    if (airdrop) {
+      console.log(`airdrop success ${airdrop}`);
+    }
+    return airdrop;
   }
 
   protected async getBalance(publicKey: string): Promise<number | undefined> {
@@ -36,12 +57,20 @@ class SolanoPlayGround {
       const sol = balance / LAMPORTS_PER_SOL;
       return sol;
     } catch (error) {
-      console.error(error);
+      console.error("unable to get balance", error);
     }
   }
 
-  private getPublicKey(): PublicKey {
-    return new PublicKey(constants.senderPublicKey);
+  getSenderPublicKey(): string {
+    return this.senderPublicKey;
+  }
+
+  getReceiverPublicKey(): string {
+    return this.receiverPublicKey;
+  }
+
+  private getPublicKeyFromSecretKey(key: string): PublicKey {
+    return getKeypairFromEnvironment(key).publicKey;
   }
 
   private getSecretKey(key: string): Uint8Array {
@@ -49,49 +78,69 @@ class SolanoPlayGround {
   }
 
   protected async creditWallet() {
+    const senderWallet = this.getSenderPublicKey();
+    const receiverWallet = this.getReceiverPublicKey();
+    const senderSecretKey = this.getSecretKey("SENDER_SECRET_KEY");
+    const senderPublicKey = this.getPublicKeyFromSecretKey("SENDER_SECRET_KEY");
     try {
-      const lamporportToSend = 500000;
-      console.log("initiating transaction");
+      const senderBalanceBefore = await this.getBalance(
+        this.getSenderPublicKey()
+      );
+
+      console.log(`Sender's balance before: ${senderBalanceBefore} SOL`);
+
+      const lamportsToSend = 5000;
+      console.log("Initiating transaction");
       const transaction = new Transaction().add(
         SystemProgram.transfer({
-          fromPubkey: new PublicKey(constants.senderPublicKey),
-          toPubkey: new PublicKey(constants.receiverPublicKey),
-          lamports: lamporportToSend,
+          fromPubkey: senderPublicKey,
+          toPubkey: new PublicKey(receiverWallet),
+          lamports: lamportsToSend,
         })
       );
-      const senderPublicKey = this.getPublicKey();
-      const senderSecretKey = this.getSecretKey("SENDER_SECRET_KEY");
+
       const signature = await sendAndConfirmTransaction(
         this.connection,
         transaction,
-        [{ publicKey: senderPublicKey, secretKey: senderSecretKey }]
+        [{ publicKey: new PublicKey(senderWallet), secretKey: senderSecretKey }]
       );
+
       console.log(
-        `💸 Finished! Sent ${lamporportToSend} to the address ${senderPublicKey}. with this signature ${signature}`
+        `💸 Finished! Sent ${lamportsToSend} to the address ${senderWallet} with this signature ${signature}`
       );
-      return this.getBalance(constants.senderPublicKey);
+
+      // Check the balance after the transaction
+      const senderBalanceAfter = await this.getBalance(
+        this.getSenderPublicKey()
+      );
+      console.log(`Sender's balance after: ${senderBalanceAfter} SOL`);
+
+      return senderBalanceAfter;
     } catch (error) {
-      console.error(error);
+      console.error("Unable to credit wallet", error);
     }
   }
 }
-
 class Test extends SolanoPlayGround {
   generate() {
     return this.generateKeyPair();
   }
   async balance() {
-    const balance = await this.getBalance(constants.senderPublicKey);
+    const balance = await this.getBalance(this.getSenderPublicKey());
     console.log(balance);
     return balance;
   }
   async credit() {
     return await this.creditWallet();
   }
+  async drop(): Promise<string> {
+    const publicKey = this.getSenderPublicKey();
+    return await this.airDrop(publicKey);
+  }
 }
 const test = new Test();
 const credit = async () => {
-  return await test.credit();
+  return await test.balance();
 };
 // balance();
 credit();
